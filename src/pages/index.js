c72881ac222f4c674aeed87317e2ff2933a0c058
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-07-17 18:24:47 +0530

    Add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,20 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import styled from "styled-components"
+
+const Index = styled.div`
+  color: whitesmoke;
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Restaurant Recommendations - Faqtly" />
-    <h1>Welcome to restaurant Recommendations</h1>
-    <p>This is a one stop website for all your restaurent recommendations.</p>
-    <p>Now go and find the restaurant that best suits you.</p>
+    <Index>
+      <h1>Welcome to restaurant Recommendations</h1>
+      <p>This is a one stop website for all your restaurent recommendations.</p>
+      <p>Now go and find the restaurant that best suits you.</p>
+    </Index>
     <Link to="/restaurants">Go to restaurant recommendations</Link> <br />
   </Layout>
 )
--- src/pages/restaurants.js
+++ src/pages/restaurants.js
@@ -4,13 +4,33 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi, Welcome to restaurant recommendations</h1>
-    <p>Good Luck!</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+import styled from 'styled-components';
+
+const Restaurent = styled.div`
+  color: #F5D020;
+`
+
+class SecondPage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      restaurents: []
+    }
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Page two" />
+        <Restaurent>
+          <h1>Hi, Welcome to restaurant recommendations</h1>
+          <p>We hope you find what you are looking for!</p>
+        </Restaurent>
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
 
 export default SecondPage
