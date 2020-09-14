81facba1bbfd32f4347361c9de63b7697a37f52f
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-09-14 10:31:55 +0530

    Update api call to display top restaurants of delhi

--- src/components/restaurant-card.component.js
+++ src/components/restaurant-card.component.js
@@ -1,131 +1,142 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import styled from 'styled-components';
+import styled from "styled-components"
 
 const Restaurant = styled.div`
-    align-items: center;
-    color: #F5D020;
+  align-items: center;
+  color: #f5d020;
+  display: flex;
+  flex-direction: column;
+  height: 360px;
+  margin-bottom: 30px;
+  overflow: hidden;
+  position: relative;
+  width: 21vw;
+
+  @media screen and (max-width: 800px) {
+    width: 60vw;
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    & .content {
+      opacity: 0.9;
+    }
+  }
+
+  .img {
+    background-position: center;
+    background-size: cover;
+    border: 1px solid #f5d020;
+    height: 90%;
+    margin-bottom: 6px;
+    width: 100%;
+  }
+
+  .content {
+    background: #232526;
+    border: 1px solid #f5d020;
+    margin-top: 120px;
+    height: 120px;
+    padding: 0 25px;
     display: flex;
     flex-direction: column;
-    height: 360px;
-    margin-bottom: 30px;
-    overflow: hidden;
-    position: relative;
-    width: 21vw;
+    align-items: center;
+    justify-content: center;
+    opacity: 0.6;
+    position: absolute;
+    width: 18vw;
 
     @media screen and (max-width: 800px) {
-        width: 60vw;
+      margin-top: 60px;
+      height: 180px;
+      opacity: 0.9;
+      width: 36vw;
     }
 
-    &:hover {
-        cursor: pointer;
-
-        & .content {
-            opacity: 0.9;
-        }
+    .title {
+      color: #f5d020;
+      font-size: 18px;
+      font-weight: bold;
+      margin: 0 6px 0;
     }
 
-    .img {
-        background-position: center;
-        background-size: cover;
-        border: 1px solid #F5D020;
-        height: 90%;
-        margin-bottom: 6px;
-        width: 100%;
+    .subtitle {
+      font-weight: lighter;
+      font-size: 18px;
+    }
+  }
+
+  .footer {
+    display: flex;
+    font-size: 18px;
+    height: 10%;
+    justify-content: space-between;
+    width: 100%;
+
+    .zomato {
+      color: #fc575e;
+      margin-bottom: 15px;
+      width: 60%;
+
+      &:hover {
+        color: #ed456d;
+      }
     }
 
-    .content {
-        background: #232526;
-        border: 1px solid #F5D020;
-        margin-top: 120px;
-        height: 120px;
-        padding: 0 25px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        opacity: 0.6;
-        position: absolute;
-        width: 18vw;
+    .maps {
+      color: #009ffd;
+      width: 40%;
 
-        @media screen and (max-width: 800px) {
-            margin-top: 60px;
-            height: 180px;
-            opacity: 0.9;
-            width: 36vw;
-        }
-
-        .title {
-            color: #F5D020;
-            font-size: 18px;
-            font-weight: bold;
-            margin: 0 6px 0;
-        }
-
-        .subtitle {
-            font-weight: lighter;
-            font-size: 18px;
-        }
+      &:hover {
+        color: #0cbaba;
+      }
     }
-
-    .footer {
-        display: flex;
-        font-size: 18px;
-        height: 10%;
-        justify-content: space-between;
-        width: 100%;
-
-
-        .zomato {
-            color: #FC575E;
-            margin-bottom: 15px;
-            width: 60%;
-
-            &:hover {
-                color: #ED456D;
-            }
-        }
-
-        .maps {
-            color: #009FFD;
-            width: 40%;
-
-            &:hover {
-                color: #0CBABA;
-            }
-        }
-    }
+  }
 `
 
 const RestaurantCard = ({ restaurant }) => {
-    const { name, average_cost_for_two, user_rating, location, url } = restaurant;
-    var { featured_image } = restaurant;
-    const { address } = location;
+  const { name, average_cost_for_two, user_rating, location, url } = restaurant
+  var { featured_image } = restaurant
+  const { address } = location
 
-    if (featured_image.length === 0) {
-        featured_image = 'https://images.unsplash.com/photo-1556694795-b6423d3d5b28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
-    }
+  if (featured_image.length === 0) {
+    featured_image =
+      "https://images.unsplash.com/photo-1556694795-b6423d3d5b28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  }
 
-    return (
-        <Restaurant>
-            <div
-                className='img' 
-                style={{
-                    backgroundImage: `url(${featured_image})`
-                }}
-            />
-            <div className='content'>
-                <h1 className='title'>{name.toUpperCase()}</h1>
-                <span className='subtitle'>Average Cost: ${average_cost_for_two}</span>
-                <span className='subtitle'>Rating: {user_rating.aggregate_rating}/5</span>
-            </div>
-            <div className='footer'>
-                <Link to={url} className='zomato'>Zomato</Link>
-                <Link to={`https://www.google.com/maps/place/${address}`} className='maps'>Google Maps</Link>
-            </div>
-        </Restaurant>
-    )
+  return (
+    <Restaurant>
+      <div
+        className="img"
+        style={{
+          backgroundImage: `url(${featured_image})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{name.toUpperCase()}</h1>
+        <span className="subtitle">
+          Average Cost: Rs {average_cost_for_two}
+        </span>
+        <span className="subtitle">
+          Rating: {user_rating.aggregate_rating}/5
+        </span>
+      </div>
+      <div className="footer">
+        <Link to={url} className="zomato">
+          Zomato
+        </Link>
+        <Link
+          to={`https://www.google.com/maps/place/${address}`}
+          className="maps"
+        >
+          Google Maps
+        </Link>
+      </div>
+    </Restaurant>
+  )
 }
 
 export default RestaurantCard
--- src/pages/restaurants.js
+++ src/pages/restaurants.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 import RestaurantCard from "../components/restaurant-card.component"
 import Spinner from "../components/spinner.component"
 
-import styled from 'styled-components';
+import styled from "styled-components"
 
 const Restaurant = styled.div`
   align-items: center;
@@ -15,17 +15,17 @@ const Restaurant = styled.div`
   justify-content: center;
 
   .header {
-      color: #F5D020;
-      font-size: 27px;
-      margin-bottom: 24px;
+    color: #f5d020;
+    font-size: 27px;
+    margin-bottom: 24px;
 
-      @media screen and (max-width: 800px) {
-        font-size: 21px;
-      }
+    @media screen and (max-width: 800px) {
+      font-size: 21px;
+    }
   }
 
   .title {
-    color: #FC575E;
+    color: #fc575e;
     font-size: 27px;
     margin-bottom: 24px;
   }
@@ -48,48 +48,55 @@ const Restaurant = styled.div`
 
 class SecondPage extends React.Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       loading: true,
       error: false,
-      restaurants: []
+      restaurants: [],
     }
   }
 
   componentDidMount() {
-    fetch('https://developers.zomato.com/api/v2.1/search?apikey=96d9bea243ddea6e8c5889b535708653')
+    fetch(
+      "https://developers.zomato.com/api/v2.1/search?city_id=1&sort=rating&apikey=96d9bea243ddea6e8c5889b535708653"
+    )
       .then(response => {
         return response.json()
       })
-        .then(data => {
-          this.setState({ 
-            restaurants: data.restaurants,
-            loading: false
-          })
+      .then(data => {
+        this.setState({
+          restaurants: data.restaurants,
+          loading: false,
         })
+      })
   }
 
   render() {
-    const { loading, restaurants } = this.state;
+    const { loading, restaurants } = this.state
 
     return (
       <Layout>
         <SEO title="Restaurant Recommendations - Faqtly" />
         <Restaurant>
-          <h1 className='header'>Welcome to restaurant recommendations by Faqtly</h1>
-          <h1 className='title'>Restaurants</h1>
+          <h1 className="header">
+            Welcome to restaurant recommendations by Faqtly
+          </h1>
+          <h1 className="title">Restaurants</h1>
           {loading ? (
             <Spinner />
           ) : (
-            <div className='items'>
-              {restaurants.map(( establishment ) => (
-                <RestaurantCard key={establishment.restaurant.id} restaurant={establishment.restaurant} />
+            <div className="items">
+              {restaurants.map(establishment => (
+                <RestaurantCard
+                  key={establishment.restaurant.id}
+                  restaurant={establishment.restaurant}
+                />
               ))}
             </div>
           )}
         </Restaurant>
-      <Link to="/">Go back to the homepage</Link>
+        <Link to="/">Go back to the homepage</Link>
       </Layout>
     )
   }
